ff8f88c294fbd12e4bebb439e8c47ce8ce96184c
mslmines <ines.msalllem@gmail.com>
2023-10-22 03:42:57 +0200

    +klc

---
+++ backend-ines/src/main/java/com/esprit/microservice/reservationservice/config/KeycloakSecurityConfig.java
@@ -0,0 +1,66 @@
+package com.esprit.microservice.reservationservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        super.configure(http);
+        http.csrf()
+                .disable()
+                .httpBasic()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/api/*").hasAuthority("user")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/candidats/user/*").hasAuthority("user")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/candidats/admin/**").hasAuthority("admin")
+                .anyRequest()
+                .authenticated();
+    }
+
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManager()throws Exception{
+        return super.authenticationManager();
+
+
+    }
+
+}
---
+++ backend-tlili/src/main/java/com/example/annoncevehicule/config/KeycloakConfig.java
@@ -0,0 +1,57 @@
+package com.example.annoncevehicule.config;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    static Keycloak keycloak=null;
+    final static String serverUrl = "http://localhost:8080/auth";
+    public final static String realm = "JobBoardKeycloacke";
+    public final static String clientId = "candidat-service";
+    final static String clientSecret = "483fdf63-3a25-467e-891d-f605f1bcacdc";
+    final static String userName = "sarra";
+    final static String password = "sarra";
+
+    public KeycloakConfig() {
+    }
+    @Bean
+    public static Keycloak getInstance() {
+        if (keycloak == null) {
+            keycloak = KeycloakBuilder.builder()
+                    .serverUrl(serverUrl)
+                    .realm(realm)
+                    .grantType(OAuth2Constants.PASSWORD)
+                    .username(userName)
+                    .password(password)
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .resteasyClient(new ResteasyClientBuilder()
+                            .connectionPoolSize(10)
+                            .build())
+                    .build();
+        }
+        return keycloak;
+    }
+
+
+
+
+
+
+
+}
+
+
