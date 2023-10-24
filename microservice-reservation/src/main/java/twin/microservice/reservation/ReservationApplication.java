34cfab2577cc335850106ccc86f0349a391ee30a
root <root@Ins>
2023-10-24 20:40:53 +0200

    ejznr

--- microservice-reservation/src/main/java/twin/microservice/reservation/ReservationApplication.java
+++ microservice-reservation/src/main/java/twin/microservice/reservation/ReservationApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class ReservationApplication {
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
     public static void main(String[] args) {
         SpringApplication.run(ReservationApplication.class, args);
     }
