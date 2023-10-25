b8be625df50c2f84d345603b8458e2383fe3b76f
y-essine <prodtf@gmail.com>
2023-10-25 02:35:48 +0100

    ignore auth for /api/auth and registration work

--- api-gateway/src/main/java/com/euroka/gateway/SpringSecurityConfig.java
+++ api-gateway/src/main/java/com/euroka/gateway/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
@@ -12,15 +13,18 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        // 
         // @formatter:off
         http
                 .authorizeExchange()
-                .anyExchange().authenticated()
+                    .pathMatchers("/api/auth/**").permitAll() // Allow unauthenticated access to /api/auth
+                    .anyExchange().authenticated()
                 .and()
                 .oauth2Login()
                 .and()
                 .oauth2ResourceServer()
                 .jwt();
+        http.csrf().disable();
         return http.build();
         // @formatter:on
     }
--- microservice-auth/routes/auth.js
+++ microservice-auth/routes/auth.js
@@ -10,13 +10,15 @@ const axios = require('axios');
 const config = {
     method: 'POST',
     loginUrl: 'http://keycloak-server:18080/auth/realms/spring-micro-main/protocol/openid-connect/token',
-    registerUrl: 'http://keycloak-server:18080/auth/realms/spring-micro-main/clients-registrations/openid-connect',
+    registerUrl: 'http://keycloak-server:18080/auth/admin/realms/spring-micro-main/users',
     clientId: 'spring-micro-gateway',
     clientSecret: '**********',
     realm: 'spring-micro-main',
-    grantType: 'password',
-    headers: {
+    loginHeaders: {
         'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    registerHeaders: {
+        'Content-Type': 'application/json'
     },
 }
 
@@ -42,8 +44,8 @@ router.post('/login', async (req, res) => {
         const options = {
             method: config.method,
             url: config.loginUrl,
-            headers: config.headers,
-            data: `grant_type=${config.grantType}&client_id=${config.clientId}&client_secret=${config.clientSecret}&username=${username}&password=${password}`,
+            headers: config.loginHeaders,
+            data: `grant_type=password&client_id=${config.clientId}&client_secret=${config.clientSecret}&username=${username}&password=${password}`,
         };
     
         await axios.request(options).then(function (response) {
@@ -61,11 +63,44 @@ router.post('/register', async (req, res) => {
     try {
         const { username, password } = req.body;
 
+        // login and get token to use in register
+        const optionsLogin = {
+            method: config.method,
+            url: config.loginUrl,
+            headers: config.loginHeaders,
+            data: `grant_type=password&client_id=${config.clientId}&client_secret=${config.clientSecret}&username=dev&password=123`,
+        };
+
+        let token = '';
+
+        await axios.request(optionsLogin).then(function (response) {
+            if (response.status != 200) {
+                return res.status(400).json({ message: 'Login error' });
+            }
+            token = response.data.access_token;
+            if (!token) {
+                return res.status(400).json({ message: 'Login error' });
+            }
+            return res.status(200).json({message: "registered successfully"} );
+        }).catch(function (error) {
+            console.error(error);
+        });
+
+        const data = {
+            "enabled": true,
+            "username": username,
+            "credentials": [{
+                "type": password,
+                "value": "123",
+                "temporary": false
+            }]
+        }
+
         const options = {
             method: config.method,
             url: config.registerUrl,
-            headers: config.headers,
-            data: `username=${username}&password=${password}`,
+            headers: {...config.registerHeaders, ...{Authorization: `Bearer ${token}`}},
+            data: data,
         };
 
         await axios.request(options).then(function (response) {
